6ac5373e60fdfc2131a8f6dd7494b4dd8604b806
xrfang <xrfang@gmail.com>
2021-11-13 20:02:46 +0800

    tweaked logging

--- guard.go
+++ guard.go
@@ -1,6 +1,7 @@
 package procguard
 
 import (
+	"path/filepath"
 	"time"
 )
 
@@ -35,25 +36,28 @@ func (g *Guardian) Run() {
 		for {
 			now := time.Now().Format("15:04:05")
 			tags := g.Slots.current(now)
-			g.log(2, "now=%s; slots=%+v", now, tags)
+			g.log(1, "now=%s; slots=%+v", now, tags)
 			for _, t := range tags {
 				for _, p := range g.Procs {
+					name := filepath.Base(p.Cmd)
 					switch p.Plan[t] {
 					case "start":
 						if p.done {
 							g.log(2, "now=%s; slot=%s; already processed", now, t)
 						} else {
-							g.log(2, "now=%s; slot=%s; starting", now, t)
+							g.log(2, "now=%s; slot=%s; start '%s'", now, t, name)
 							if err := p.start(); err == nil {
 								p.done = true
 							}
 						}
 					case "stop":
-						g.log(2, "now=%s; slot=%s; terminating", now, t)
+						g.log(2, "now=%s; slot=%s; terminate '%s'", now, t, name)
 						p.stop()
 					case "keepalive":
-						g.log(2, "now=%s; slot=%s; keepalive", now, t)
+						g.log(2, "now=%s; slot=%s; keepalive '%s'", now, t, name)
 						p.start()
+					default:
+						g.log(2, "now=%s; slot=%s; no action for '%s'", now, t, name)
 					}
 				}
 			}
